c37911d1c8570d95e01f8b39b8447c82899b1350
jordanmceachern <jormceachern@gmail.com>
2018-12-18 21:37:29 +0000

    navbar complete

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,26 +12,24 @@ toggleHandler = () => {
     
 render(){
     return (
-        <React.Fragment>
-            <div id="header">
-                <div className="heading">
-                    <div id="hamburger" onClick={this.toggleHandler}>
-                        <hr />
-                        <hr />
-                        <hr />
-                    </div>
-                    <a id="home" href="#">william mcquinn</a>
-                </div>
-                <div id="footer" className="heading mobile hideThis" onClick={this.toggleHandler}>
-                    <a 
-                        href="https://www.facebook.com/williammcquinn7" 
-                        target="_blank" 
-                        rel="noopener noreferrer">
-                        <img id="fb" src={facebook} alt="link to facebook"/>facebook page
-                    </a>
-                </div>
+        <nav>
+            <div id="hamburger" onClick={this.toggleHandler}>
+                <hr />
+                <hr />
+                <hr />
             </div>
-        </React.Fragment>
+            <a id="home" href="#top"><p>William McQuinn</p></a>
+            <a className="heading mobile hideThis" href="#photos">photos</a>
+            <a className="heading mobile hideThis" href="#videos">videos</a>
+            <div id="facebook" className="heading mobile hideThis" onClick={this.toggleHandler}>
+                <a 
+                    href="https://www.facebook.com/williammcquinn7" 
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                    <img id="fb" src={facebook} alt="link to facebook"/>facebook page
+                </a>
+            </div>
+        </nav>
         )
     }
 }
