0c3d7b7ba91eb7d147e9aee26d2593fdd4294735
jordanmceachern <jormceachern@gmail.com>
2019-05-06 15:06:53 -0300

    added new photos and fixed config site meta data

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    siteMetadata: {
+      title: `William McQuinn`,
+      siteUrl: `https://williammcquinn.com`,
+      description: `William is an event videographer trained by director David McQuinn, and 
+      resides in Fredericton NB. Working alongside McQuinn Media for over 5 years 
+      and educated by Hollywood professionals, William knows how to capture a 
+      moment through the eye of the lens.`
+    },
+    plugins: [`gatsby-plugin-react-helmet`]
+  }
--- src/components/imageLoader/imageLoader.js
+++ src/components/imageLoader/imageLoader.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import './imageLoader.css'
 import Layout from '../layout'
-import c1light from './filminglight.jpg'
-import c2light from './carousel_2light.jpg'
+import c1light from './carousel_2light.jpg'
+import c2light from './filminglight.jpg'
 import c3light from './carousel_3light.jpg'
 import c4light from './carousel_4light.jpg'
 import c5light from './carousel_5light.jpg'
 import c6light from './carousel_6light.jpg'
 import c7light from './carousel_7light.jpg'
-import c1 from './filming.jpg'
-import c2 from './carousel_2.jpg'
+import c8light from './carousel_8light.jpg'
+import c9light from './carousel_9light.jpg'
+import c10light from './carousel_10light.jpg'
+import c11light from './carousel_11light.jpg'
+import c1 from './carousel_2.jpg'
+import c2 from './filming.jpg'
 import c3 from './carousel_3.jpg'
 import c4 from './carousel_4.jpg'
 import c5 from './carousel_5.jpg'
 import c6 from './carousel_6.jpg'
 import c7 from './carousel_7.jpg'
+import c8 from './carousel_8.jpg'
+import c9 from './carousel_9.jpg'
+import c10 from './carousel_10.jpg'
+import c11 from './carousel_11.jpg'
 
 class ImageLoader extends React.Component {
   state = {
-    photos: [c2light, c1light, c3light, c4light, c5light, c6light, c7light],
-    jpg: [c2, c1, c3, c4, c5, c6, c7]
+    photos: [c8light, c9light, c10light, c11light, c1light, c2light, c3light, c4light, c5light, c6light, c7light],
+    jpg: [c8, c9, c10, c11, c1, c2, c3, c4, c5, c6, c7]
   }
   
   componentDidMount(){
