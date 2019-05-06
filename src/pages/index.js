c2a9986bc7df70370e4a788b0d3b2d7c6f02cb56
jordanmceachern <jormceachern@gmail.com>
2019-05-06 13:42:02 -0300

    added site meta data and react helment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Layout from '../components/layout'
 import ImageLoader from '../components/imageLoader/imageLoader'
+import { Helmet } from 'react-helmet'
 
 export default () => {
-    return (
-        <div className="App">
-            <ImageLoader />
-            <Layout />            
-        </div>
-    )
+  return (
+    <div className="App">
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>William McQuinn</title>
+        <meta name='description' content='William is an event videographer trained by director David McQuinn, and 
+                    resides in Fredericton NB. Working alongside McQuinn Media for over 5 years 
+                    and educated by Hollywood professionals, William knows how to capture a 
+                    moment through the eye of the lens.'></meta>
+        <link rel='canonical' href='https://williammcquinn.com' />
+      </Helmet>
+      <ImageLoader />
+      <Layout />            
+    </div>
+  )
 }
